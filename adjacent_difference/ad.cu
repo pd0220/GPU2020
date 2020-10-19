5228b0d7bc781e632b32df39cf9a1f0009b68716
Dávid Pesznyák <dpesznyak@student.elte.hu>
2020-10-19 15:32:55 +0200

    two buffers for kernel

--- adjacent_difference/ad.cu
+++ adjacent_difference/ad.cu
@@ -6,16 +6,20 @@
 #include <random>
 
 // kernel
-__global__ void adjacent_difference(int n, float *x)
+__global__ void adjacent_difference(int n, float *x, float *y)
 {
 	// data indices to blocks
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	// run algorithm
 	// first element ~ do nothing
 	// rest of the elements ~ compute differences
-	if (i < n && i != 0)
+	if (i == 0)
 	{
-		x[i] = x[i] - x[i - 1];
+		y[i] = x[i];
+	}
+	else
+	{
+		y[i] = x[i] - x[i - 1];
 	}
 }
 
@@ -26,21 +30,26 @@ int main(int, char **)
 	std::random_device rd{};
 	std::mt19937 gen(rd());
 	std::normal_distribution<float> distr(-10.f, 10.f);
-
+	// random generator lambda
 	auto rand = [&distr, &gen]() {
 		return (float)distr(gen);
 	};
 
+	// test variable
+	int test = 1024;
+
 	// size
-	size_t size = (int)(1000 * 1000);
+	size_t size = (int)(test * test);
 
 	// test vectors
 	std::vector<float> XVec(size);
+	std::vector<float> YVec(size);
 	std::generate(XVec.begin(), XVec.end(), rand);
-	std::vector<float> ResultVec(XVec.size());
+	std::vector<float> ResultVec(size);
 
 	// vectors for devcie
 	float *devX = nullptr;
+	float *devY = nullptr;
 
 	// memory allocation on device
 	cudaError_t err = cudaSuccess;
@@ -50,21 +59,33 @@ int main(int, char **)
 		std::cout << "Error allocating CUDA memory (X): " << cudaGetErrorString(err) << std::endl;
 		return -1;
 	}
-
-	// copy data to onto device
+	err = cudaMalloc((void **)&devY, size * sizeof(float));
+	if (err != cudaSuccess)
+	{
+		std::cout << "Error allocating CUDA memory (Y): " << cudaGetErrorString(err) << std::endl;
+		return -1;
+	}
+	
+	// copy data onto device
 	err = cudaMemcpy(devX, XVec.data(), size * sizeof(float), cudaMemcpyHostToDevice);
 	if (err != cudaSuccess)
 	{
 		std::cout << "Error copying memory to device (X): " << cudaGetErrorString(err) << std::endl;
 		return -1;
 	}
+	err = cudaMemcpy(devY, YVec.data(), size * sizeof(float), cudaMemcpyHostToDevice);
+	if (err != cudaSuccess)
+	{
+		std::cout << "Error copying memory to device (Y): " << cudaGetErrorString(err) << std::endl;
+		return -1;
+	}
 
 	// grid and block dimensions
-	dim3 dimGrid(size / 1000, 1);
-	dim3 dimBlock(1000, 1);
+	dim3 dimGrid(size / test, 1);
+	dim3 dimBlock(test, 1);
 
 	// start kernel
-	adjacent_difference<<<dimGrid, dimBlock>>>((int)size, devX);
+	adjacent_difference<<<dimGrid, dimBlock>>>((int)size, devX, devY);
 
 	// get errors from run
 	err = cudaGetLastError();
@@ -75,7 +96,7 @@ int main(int, char **)
 	}
 
 	// copy data from device
-	err = cudaMemcpy(ResultVec.data(), devX, size * sizeof(float), cudaMemcpyDeviceToHost);
+	err = cudaMemcpy(ResultVec.data(), devY, size * sizeof(float), cudaMemcpyDeviceToHost);
 	if (err != cudaSuccess)
 	{
 		std::cout << "Error copying memory to host: " << cudaGetErrorString(err) << std::endl;
@@ -87,6 +108,12 @@ int main(int, char **)
 	if (err != cudaSuccess)
 	{
 		std::cout << "Error freeing allocation (X): " << cudaGetErrorString(err) << std::endl;
+		return -1;
+	}
+	err = cudaFree(devY);
+	if (err != cudaSuccess)
+	{
+		std::cout << "Error freeing allocation (Y): " << cudaGetErrorString(err) << std::endl;
 		return -1;
 	}
 
